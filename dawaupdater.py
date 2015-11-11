1aae869bd81171d3860a3472be7edce2c047f112
Andrius Ordojan <andrius.or@gmail.com>
2015-11-11 15:57:19 +0100

    removed clutter from the codebase

--- dawaupdater.py
+++ dawaupdater.py
@@ -16,10 +16,8 @@ Options:
 from docopt import docopt
 
 import json
-import time
 import concurrent.futures
 import csv
-from queue import Queue
 from pprint import pprint
 
 from models import *
@@ -33,54 +31,13 @@ import config
 import shutil
 import tempdata
 
-
-SERVER_URL = 'http://dawa.aws.dk/'
-
 wgs84 = pyproj.Proj(init='epsg:4326')
 etrs89 = pyproj.Proj(init='epsg:25832')
 
 vejstykker_service = None
 address_service = None
 
-
-def create_houseunit(data):
-    houseunit = {}
-
-    houseunit['adgangsadresse_uuid'] = data['id']
-    houseunit['kommuneid'] = data['kommunekode']
-
-    houseunit['roadid'] = data['vejkode']
-    houseunit['roadname'] = vejstykker_service.get_road_name_from_road_id_and_commune_id(data['vejkode'],
-                                                                                         data['kommunekode'])
-
-    house_id = data['husnr']
-    houseunit['houseid'] = house_id
-
-    house_number = re.findall(r'\d+', house_id)[0]
-    if int(house_number) % 2 == 0:
-        houseunit['equalno'] = 1
-    else:
-        houseunit['equalno'] = 0
-
-    x = data['etrs89koordinat_øst']
-    y = data['etrs89koordinat_nord']
-
-    wgs84_coordinates = pyproj.transform(etrs89, wgs84, x, y)
-    houseunit['x'] = wgs84_coordinates[0]
-    houseunit['y'] = wgs84_coordinates[1]
-
-    houseunit['doorcount'] = address_service.get_door_count_from_adgangsadresseid(data['id'])
-    houseunit['zip'] = data['postnr']
-
-    parish = address_service.get_parish_from_adgangsadresseid(data['id'])
-    houseunit['sognenr'] = parish['kode']
-    houseunit['sognenavn'] = parish['navn']
-
-    valgkredskode = address_service.get_political_district_id_from_adgangsadresseid(data['id'])
-    houseunit['valgkreds'] = valgkredskode
-
-    return houseunit
-
+SERVER_URL = 'http://dawa.aws.dk/'
 
 def import_commune_information():
     print('importing commune information...')
@@ -168,130 +125,71 @@ def import_area_information():
         executor.submit(SamArea.insert_many(get_parishes()).execute())
         executor.submit(SamArea.insert_many(get_electoral_districts()).execute())
 
+def create_houseunit(data):
+    houseunit = {}
 
-def import_address_information():
+    houseunit['adgangsadresse_uuid'] = data['id']
+    houseunit['kommuneid'] = data['kommunekode']
+
+    houseunit['roadid'] = data['vejkode']
+    houseunit['roadname'] = vejstykker_service.get_road_name_from_road_id_and_commune_id(data['vejkode'],
+                                                                                         data['kommunekode'])
+
+    house_id = data['husnr']
+    houseunit['houseid'] = house_id
+
+    house_number = re.findall(r'\d+', house_id)[0]
+    if int(house_number) % 2 == 0:
+        houseunit['equalno'] = 1
+    else:
+        houseunit['equalno'] = 0
+
+    x = data['etrs89koordinat_øst']
+    y = data['etrs89koordinat_nord']
+
+    wgs84_coordinates = pyproj.transform(etrs89, wgs84, x, y)
+    houseunit['x'] = wgs84_coordinates[0]
+    houseunit['y'] = wgs84_coordinates[1]
+
+    houseunit['doorcount'] = address_service.get_door_count_from_adgangsadresseid(data['id'])
+    houseunit['zip'] = data['postnr']
+
+    parish = address_service.get_parish_from_adgangsadresseid(data['id'])
+    houseunit['sognenr'] = parish['kode']
+    houseunit['sognenavn'] = parish['navn']
+
+    valgkredskode = address_service.get_political_district_id_from_adgangsadresseid(data['id'])
+    houseunit['valgkreds'] = valgkredskode
+
+    return houseunit
+
+def freshimport():
     print('importing address info')
 
-    process_queue = Queue()
-    write_queue = Queue()
+    with open(config.ADDRESS_ACCESS_DATA, encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
 
-    def read_address_data():
-        print('reading address data...')
+        next(reader)  # skip header
 
-        last_index = 0
+        for index, row in enumerate(reader):
+            if index % 50000 == 0:
+                print('records read: {0}'.format(index))
 
-        with open(config.ADDRESS_ACCESS_DATA, encoding='utf-8') as csvfile:
-            output = []
-            reader = csv.reader(csvfile)
+            valid_address = True
 
-            next(reader)  # skip header
+            address = {'id': row[0], 'kommunekode': row[2], 'vejkode': row[3], 'husnr': row[4], 'postnr': row[6],
+                       'etrs89koordinat_øst': row[13], 'etrs89koordinat_nord': row[14]}
 
-            for index, row in enumerate(reader):
-                valid_address = True
+            for v in address.values():
+                if not v:
+                    valid_address = False
+                    break
 
-                last_index = index
-
-                if index != 0 and index % config.CHUNK_SIZE == 0:
-                    process_queue.put(output)
-                    print('process queue: item added; size: {0}'.format(process_queue.qsize()))
-                    output = []
-                    while process_queue.qsize() >= 10:
-                        print('Process queue full')
-                        time.sleep(30)
-
-                address = {'id': row[0], 'kommunekode': row[2], 'vejkode': row[3], 'husnr': row[4], 'postnr': row[6],
-                           'etrs89koordinat_øst': row[13], 'etrs89koordinat_nord': row[14]}
-
-                for v in address.values():
-                    if not v:
-                        valid_address = False
-                        break
-
-                if valid_address:
-                    output.append(address)
-
-            if output:
-                process_queue.put(output)
-
-        print('ending address reading. Last index {0}'.format(last_index))
-        return 'reading address data done.'
-
-    def process_address_data():
-        print('processing address data block...')
-
-        stop = False
-        output = []
-
-        while not stop:
-            addresses = process_queue.get()
-
-            for index, address in enumerate(addresses):
-                if index != 0 and (index + 1) % config.CHUNK_SIZE == 0:
-                    write_queue.put(output)
-                    output = []
-
-                    print('write queue: item added; size: {0}'.format(write_queue.qsize()))
-
-                    while write_queue.qsize() >= 10:
-                        print('Write queue full')
-                        time.sleep(30)
-
+            if valid_address:
                 houseunit = create_houseunit(address)
-                output.append(houseunit)
-
-            print('processing address data, queue size {0}'.format(process_queue.qsize()))
-            if process_queue.qsize() == 0:
-                print('process queue empty processor shutting off')
-                stop = True
-
-        return 'processing address data block done.'
-
-    def write_houseunits_to_database():
-        print('writing houseunits to database...')
-
-        stop = False
-        records_written = 0
-
-        while not stop:
-            houseunits = write_queue.get()
-
-            for houseunit in houseunits:
                 SamHouseunits.create(**houseunit)
 
-                records_written += 1
-
-                if records_written % 10000 == 0:
-                    print('{0} records written'.format(records_written))
-
-            print('writing address data, queue size {0}'.format(write_queue.qsize()))
-            if write_queue.empty():
-                print('write queue empty writer shutting off')
-                stop = True
-
-        return 'writing houseunits to database done.'
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        f = executor.submit(read_address_data)
-        time.sleep(5)
-
-        futures = [
-            f,
-
-            executor.submit(process_address_data),
-            executor.submit(process_address_data),
-            executor.submit(write_houseunits_to_database),
-            executor.submit(write_houseunits_to_database),
-            executor.submit(write_houseunits_to_database)
-        ]
-
-        for future in concurrent.futures.as_completed(futures):
-            try:
-                print(future.result())
-            except Exception as e:
-                template = 'exception of type {0} occurred. \narguments:\n{1} \nmessage: {2}'
-                message = template.format(type(e).__name__, e.args, e)
-                print(message)
-
+    print('done importing address info')
 
 def update_address_information():
     print('updating address info')
@@ -360,7 +258,6 @@ def update_address_information():
             event_handler(event)
 
     print('done updating')
-
 
 def initialize(is_update):
     def get_current_update():
@@ -484,7 +381,6 @@ def initialize(is_update):
     address_service = AddressService()
     print('done')
 
-
 def register_update():
     print('registering an update...')
 
@@ -499,7 +395,7 @@ def register_update():
 def updates_available():
     print('checking for updates...')
 
-    response = requests.get('http://dawa.aws.dk/replikering/senestesekvensnummer')
+    response = requests.get('{0}replikering/senestesekvensnummer'.format(SERVER_URL))
     update = response.json()
 
     sekvensnummertil = update['sekvensnummer']
@@ -531,7 +427,7 @@ def main(arguments):
         initialize(is_update)
         import_commune_information()
         import_area_information()
-        import_address_information()
+        freshimport()
 
     register_update()
 
